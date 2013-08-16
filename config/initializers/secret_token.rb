44cb48b288479e536e066584ccb170efa4115740
Dan Pickett <dan.pickett@launchware.com>
2013-08-16 11:30:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BananaStand::Application.config.secret_key_base = '3cf7df8be05811cbb58b559bba7e0daaabd5f258e5be26fd2f0235ff125e7a8559e82fc41c4e8018d5736e1b0de86b9f8909a6415e1075424cb518e5316f9ad1'
