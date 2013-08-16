7b63b627c0c734e93641cbd265a0a698073f948f
Dan Pickett <dan.pickett@launchware.com>
2013-08-16 14:17:00 -0400

    morning demonstration of css

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ BananaStand::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'test' => 'welcome#test'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
